e7971ff371d2e656ccb03c2867a9c17a2ed0dbea
Priyanka7011 <pk3201134@gmail.com>
2021-06-24 01:48:45 +0530

    horizontal scroller

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
+import $ from 'jquery';
 import './App.css';
+import React,{useEffect} from 'react'
 
 function App() {
+  useEffect(()=>{
+
+    $(document).ready(function() {
+      window.$('#container-horizontal').pagepiling({
+          menu: null,
+            direction: 'horizontal',
+            verticalCentered: true,
+            sectionsColor: [],
+            anchors: [],
+            scrollingSpeed: 700,
+            easing: 'swing',
+            loopBottom: false,
+            loopTop: false,
+            css3: true,
+            navigation: {
+                'textColor': '#000',
+                'bulletsColor': '#000',
+                'position': 'right',
+                'tooltips': ['section1', 'section2', 'section3', 'section4']
+            },
+             normalScrollElements: null,
+            normalScrollElementTouchThreshold: 5,
+            touchSensitivity: 5,
+            keyboardScrolling: true,
+            sectionSelector: '.section',
+            animateAnchor: false,
+    
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+      });
+    });
+    
+
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container-horizontal">
+
+      <div class='section'>one</div>
+      <div class='section'>two</div>
+      <div class='section'>three</div>
+      <div class='section'>four</div>
+      <div class='section'>five</div>
+ 
     </div>
   );
 }
